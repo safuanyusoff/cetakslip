a3ab6824728c9785be718eb1972f48b222f83ba7
safuanyusoff <42072955+safuanyusoff@users.noreply.github.com>
2018-08-04 01:22:52 +0800

    Initial commit

---
+++ print.php
@@ -0,0 +1,37 @@
+
+
+<html>
+<body>
+<div>
+
+<div>
+<?php echo $_POST["1"]; ?><br>
+<?php echo $_POST["2"]; ?><br>
+<?php echo $_POST["3"]; ?><br>
+<?php echo $_POST["4"]; ?><br>
+<?php echo $_POST["5"]; ?><br>
+<?php echo $_POST["6"]; ?><br>
+</div>
+
+
+<?php echo $_POST["7"]; ?><br>
+<?php echo $_POST["8"]; ?><br>
+<?php echo $_POST["9"]; ?><br>
+<?php echo $_POST["10"]; ?><br>
+<?php echo $_POST["11"]; ?><br>
+<?php echo $_POST["12"]; ?><br>
+
+
+<div>
+<?php echo $_POST["13"]; ?><br>
+<?php echo $_POST["14"]; ?><br>
+<?php echo $_POST["15"]; ?><br>
+<?php echo $_POST["16"]; ?><br>
+<?php echo $_POST["17"]; ?><br>
+<?php echo $_POST["18"]; ?><br>
+<?php echo $_POST["19"]; ?><br>
+</div>
+</div>
+
+</body>
+</html>
